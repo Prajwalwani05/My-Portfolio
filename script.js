bcb408aaa9b074a93410fcc6b28c8f5899615de3
Prajwal Wani <prajwalwani05@gmail.com>
2023-09-27 20:32:14 +0530

    initital

---
+++ script.js
@@ -0,0 +1,16 @@
+// window.location.reload(`${document.getElementById('sec1')}`)
+let home = document.getElementById('sec1');
+let about = document.getElementById('sec2');
+let project = document.getElementById('sec3');
+let contact = document.getElementById('sec4');
+
+let one = document.getElementById('one');
+let two = document.getElementById('two');
+let three = document.getElementById('three');
+let four = document.getElementById('four');
+
+window.addEventListener('load', ()=>{
+    window.scrollTo(0 , home);
+});
+
+
