7c959274d91e88517a80f1c7680c865e8064b4ac
Prajwal Wani <prajwalwani05@gmail.com>
2023-10-09 08:43:43 +0530

    final commit

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ let two = document.getElementById('two');
 let three = document.getElementById('three');
 let four = document.getElementById('four');
 
-// window.addEventListener('load', ()=>{
-//     window.scrollTo(0 , home);
-// });
+window.addEventListener('load', ()=>{
+    window.scrollTo(0 , home);
+});
 
 
 let resume = 'https://drive.google.com/file/d/1zj7O-FfeY5f5xpncMJ92h2EN_gtpoCVl/view?usp=drive_link';
@@ -25,48 +25,44 @@ document.getElementById("resumeBtn").addEventListener('click' , ()=>{
 // var tl = gsap.timeline();
 gsap.registerPlugin(ScrollTrigger);
 
-TweenMax.from(".headerSec1 .logo",{
-    x:-20,
-    opacity:0,
-    ease:Expo.easeInOut,
-})
+// TweenMax.from(".headerSec1 .logo",{
+//     x:-20,
+//     opacity:0,
+//     ease:Expo.easeInOut,
+// })
 
-TweenMax.staggerFrom("nav ul li" , 1,{
-    x:-20,
-    opacity:0,
-    delay:.2,
-    ease:Power3.easeInOut,
-},0.08)
+// TweenMax.staggerFrom("nav ul li" , 1,{
+//     x:-20,
+//     opacity:0,
+//     delay:.2,
+//     ease:Power3.easeInOut,
+// },0.08)
  
 
 TweenMax.from(".textContent1 .heading1 , .textContent1 p , .textContent1 .socialLinks" ,1, {
     y:20,
-    delay:1,
+    delay:.3,
     opacity:0,
-    ease:Expo.easeInOut,
+    ease:Expo.easeInOut
 })
+
+
     TweenMax.from(".secondDiv ,.icons>ul>li", 1, {
         y:20,
         opacity:0,
         delay:1.6,
         ease:Expo.easeInOut,
+        stagger:.2,
     })
 
-  TweenMax.from(".imgDiv",2, {
-      y:-800,
-      opacity: 0,
-      delay: .5, 
+  TweenMax.from(".imgimg",1, {
+    y: 20,
+    delay:.8,
+    // duration: 1,
+    ease:Expo.easeInOut,
+    opacity:0,
     });
 
-    // gsap.utils.toArray('.section').forEach(section =>{
-    //     ScrollTrigger.create({
-    //         trigger: section,
-    //         start: 'top top',
-    //         end: 'bottom top',
-    //         pin: true,
-    //         pinSpacing: true,
-    //     });
-    // });
     gsap.utils.toArray('.section').forEach(section => {
         const trigger = section;
         const endTrigger = section.nextElementSibling || null; // Assumes the next sibling is the end trigger
@@ -82,40 +78,51 @@ TweenMax.from(".textContent1 .heading1 , .textContent1 p , .textContent1 .social
         });
     });
 
-/*
-//     tl.from(".sec2 .aboutImage , .sec2 .aboutContent", {
-//         scale:.5,
-//         opacity:0,
-//         duration:.7,
-        
-//         scrollTrigger:{
-//             trigger:".sec2 .aboutImage",
-//             scroller:"body",
-//             start:"top 60%",
-//         }
-//     })
-//   tl.from(".sec3 .projectHeading" ,{
-//     y:-100,
-//     opacity:0,
-//     duration:.5,
-//     delay:.7,
-//     stagger:0.1,
-//     ease:"back",
-//     scrollTrigger:{
-//             trigger:".sec3 .projectHeading h1",
-//             scroller:"body",
-//         }
-//   })
 
-// tl.from(".sec3 .card",{
-//      y:-100,
-//     opacity:0,
-//     duration:1,
-//     stagger:0.1,
-//     ease:"back",
-//     scrollTrigger:{
-//             trigger:".sec3 .projectHeading h1",
-//             scroller:"body",
-//         }
+    TweenMax.from(".sec2 .aboutImage , .sec2 .aboutContent", {
+        scale:.5,
+        opacity:0,
+        duration:.7,
+        
+        scrollTrigger:{
+            trigger:".sec2 .aboutImage",
+            scroller:"body",
+            start:"top 60%",
+        }
+    })
+    TweenMax.from(".sec3 .projectHeading" ,{
+    y:30,
+    opacity:0,
+    // duration:.5,
+    delay:.7,
+    stagger:0.1,
+    ease:"bounce",
+    scrollTrigger:{
+            trigger:".sec3 .projectHeading h1",
+            scroller:"body",
+        }
+  })
+
+  TweenMax.from(".sec3 .card",{
+     y:40,
+    opacity:0,
+    delay:.5,
+    // duration:1,
+    stagger:0.1,
+    ease:"bounce",
+    scrollTrigger:{
+            trigger:".sec3 .projectHeading h1",
+            scroller:"body",
+        }
+})
+
+// TweenMax.from(".sec4",{
+//       scale:.5,
+// opacity:0,
+// duration:.7,
+
+// scrollTrigger:{
+//     trigger:".sec2 .aboutImage",
+//     scroller:"body",
+// }
 // })
-*/
